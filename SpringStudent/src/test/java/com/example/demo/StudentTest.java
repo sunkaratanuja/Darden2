175ab5af887f21fcd137755e9bc1940b60870804
tanuvenk <tanuvenk@LIN-PF25NZ9Q.corp.capgemini.com>
2023-11-23 11:38:23 +0530

    yes commit

--- SpringStudent/src/test/java/com/example/demo/StudentTest.java
+++ SpringStudent/src/test/java/com/example/demo/StudentTest.java
@@ -6,12 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.demo.model.Student;
+import com.example.demo.repo.StudentRepository;
 import com.example.demo.service.StudentService;
 
 @WebMvcTest(value = StudentService.class)
@@ -20,14 +22,25 @@ public class StudentTest {
 	MockMvc mockMvc;
 	@MockBean
 	StudentService studentService;
+	
+	@Mock
+	private StudentRepository studentRepository;
 
 	@Test
 	void testGetByName() {
 
 		List<Student> list = new ArrayList<>();
 
-		Student student = new Student(25,"chandu",34333);
+		Student student = new Student(25,"tanuja",34333);
 		list.add(student);
-		when(studentService.findByName("chandu")).thenReturn(list);
+		when(studentService.findByName("tanuja")).thenReturn(list);
+	}
+	@Test
+	public void testSaveStudents() {
+		Student student=new Student(0, null, 0);
+		student.setName("rachana");
+		student.setAge(23);
+		student.setSalary(30000);
+		when(studentRepository.save(student)).thenReturn(student);
 	}
 }
